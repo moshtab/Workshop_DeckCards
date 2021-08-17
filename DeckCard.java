043dda75353d7b6af746259dcbf6151146f506b5
moshtab <87015799+moshtab@users.noreply.github.com>
2021-08-17 10:21:23 +0530

    Update DeckCard.java

--- DeckCard.java
+++ DeckCard.java
@@ -1,7 +1,5 @@
 package workshop;
 
-import java.util.Random;
-
 public class DeckCard {
 	static String[] Clubs = { "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9","C10" , "CJack", "CQueen", "CKing", "CAce" };
 	static String[] Diamonds = {"D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9","D10",  "DJack", "DQueen", "DKing", "DAce" };
